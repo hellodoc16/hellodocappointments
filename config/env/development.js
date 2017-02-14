65b59c1d3d971218700e70098c25b7e973abbbc3
confident <confidenthospital@gmail.com>
2017-02-14 17:38:10 +0530

    changes

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://hd:hd4321@ds145659.mlab.com:45659/hellodoc_appoints',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
     options: {
       user: '',
       pass: ''
