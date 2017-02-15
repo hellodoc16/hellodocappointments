35c96f127e060bcee7c1728a77379e2f3c4613ca
confident <confidenthospital@gmail.com>
2017-02-15 15:56:23 +0530

    changes

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -25,7 +25,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
     };
 
     $scope.signin = function () {
-      $http.post('/api/auth/signin', $scope.credentials).success(function (response) {
+      $http.post('/api/auth/signin', $scope.credentials).response(function (response) {
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
 
