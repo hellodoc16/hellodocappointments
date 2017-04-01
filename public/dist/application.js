5c4b5df4a21ede082a5c90466b9fc505868882bc
confident <confidenthospital@gmail.com>
2017-04-01 12:13:48 +0530

    Changes

--- public/dist/application.js
+++ public/dist/application.js
@@ -2545,11 +2545,11 @@ angular.module('users').controller('ChangePasswordController', ['$scope', '$http
     $scope.changeUserPassword = function () {
       $scope.success = $scope.error = null;
 
-      $http.post('/api/users/password', $scope.passwordDetails).success(function successCallback(response) {
+      $http.post('/api/users/password', $scope.passwordDetails).success(function (response) {
         // If successful show success message and clear form
         $scope.success = true;
         $scope.passwordDetails = null;
-      }).error(function successCallback(response) {
+      }).error(function (response) {
         $scope.error = response.message;
       });
     };
