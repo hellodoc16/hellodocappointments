33f607689e028a34132e736da803222f36dd8882
confident <confidenthospital@gmail.com>
2017-04-01 12:09:27 +0530

    Changes

--- modules/users/client/controllers/settings/change-password.client.controller.js
+++ modules/users/client/controllers/settings/change-password.client.controller.js
@@ -8,11 +8,11 @@ angular.module('users').controller('ChangePasswordController', ['$scope', '$http
     $scope.changeUserPassword = function () {
       $scope.success = $scope.error = null;
 
-      $http.post('/api/users/password', $scope.passwordDetails).success(function (response) {
+      $http.post('/api/users/password', $scope.passwordDetails).success(function successCallback(response) {
         // If successful show success message and clear form
         $scope.success = true;
         $scope.passwordDetails = null;
-      }).error(function (response) {
+      }).error(function successCallback(response) {
         $scope.error = response.message;
       });
     };
