7c0708af32a8b27531016eb214274eea2717af69
confident <confidenthospital@gmail.com>
2017-04-03 14:46:00 +0530

    Changes

--- public/dist/application.js
+++ public/dist/application.js
@@ -2451,25 +2451,25 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
     }
 
     $scope.signup = function () {
-      $http.post('/api/auth/signup', $scope.credentials).then(function (response) {
+      $http.post('/api/auth/signup', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
-        $scope.authentication.user = response.data;
+        $scope.authentication.user = response;
 
         // And redirect to the previous or home page
         $state.go($state.previous.state.name || 'events.main', $state.previous.params);
-      }, function (response) {
+      }).error(function (response) {
         $scope.error = response.message;
       });
     };
 
     $scope.signin = function () {
-      $http.post('/api/auth/signin', $scope.credentials).then(function (response) {
+      $http.post('/api/auth/signin', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
-        $scope.authentication.user = response.data;
+        $scope.authentication.user = response;
 
         // And redirect to the previous or home page
         $state.go($state.previous.state.name || 'events.main', $state.previous.params);
-      }, function (response) {
+      }).error(function (response) {
         $scope.error = response.message;
       });
     };
@@ -2487,7 +2487,6 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
     };
   }
 ]);
-
 'use strict';
 
 angular.module('users').controller('PasswordController', ['$scope', '$stateParams', '$http', '$location', 'Authentication',
